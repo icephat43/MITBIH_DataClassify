b3601b4df13e783a475be7bbeae637d5a7e32bc3
icephat43 <wmcc43@gmail.com>
2015-06-03 17:02:27 +0800

    -try svm lib using

--- src/MITBIH_DataClassify.java
+++ src/MITBIH_DataClassify.java
@@ -1,7 +1,6 @@
 import java.io.IOException;
 import java.text.ParseException;
 
-
 public class MITBIH_DataClassify {
 
 	/**
@@ -12,6 +11,18 @@ public class MITBIH_DataClassify {
 	public static void main(String[] args) throws IOException, ParseException {
 		TransFormer trans = new TransFormer();
 		trans.TransForm();
-		
+		SVM();
+	}
+	
+	private static void SVM() throws IOException{
+		String[] trainpara = {
+				"./Data/106_extracted.txt", //training data
+				"./Data/106_model.txt"};//output model
+		String[] testpara = {
+				"./Data/106_extracted.txt", //testing data, though I know I shouldn't use training data as testing data
+				"./Data/106_model.txt", //using model
+				"./Data/out_consiquence"}; //consequence
+		svm_train.main(trainpara);
+		svm_predict.main(testpara);
 	}
 }
